d093fc49cda95dcb5935c438bdee784c19ee1c43
misterbombay <87628148+misterbombay@users.noreply.github.com>
2025-12-15 18:05:48 +0530

    Create IMCSAccordionWebpartProps.ts

---
+++ IMCSAccordionWebpartProps.ts
@@ -0,0 +1,8 @@
+import { IAccordionHeaderProps } from './components/IAccordionHeaderProps';
+import { IAccordionBodyProps } from './components/IAccordionBodyProps';
+
+
+export interface IMCSAccordionWebpartProps {
+header: IAccordionHeaderProps;
+body: IAccordionBodyProps;
+}
